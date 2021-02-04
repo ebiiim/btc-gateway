7aa761befa7a4c8e9f1678815e60a5d9202794e0
Shunsuke Ise <ise@ebiiim.com>
2021-02-05 05:13:57 +0900

    gw: add BTC interface

---
+++ gw/btc.go
@@ -0,0 +1,19 @@
+package gw
+
+import (
+	"context"
+
+	"github.com/ebiiim/btc-gateway/btc"
+	"github.com/ebiiim/btc-gateway/model"
+)
+
+// BTC provides features to send and get anchor.
+// This interface is not responsible for which Bitcoin wallet is used.
+type BTC interface {
+	// PutAnchor anchors the given Anchor by sending a Bitcoin transaction and returns its ID.
+	PutAnchor(ctx context.Context, a *model.Anchor) ([]byte, error)
+	// GetAnchor returns an AnchorRecord by searching the given Bitcoin transaction ID and parsing its data.
+	GetAnchor(ctx context.Context, btctx []byte) (*model.AnchorRecord, error)
+}
+
+var _ BTC = (*btc.BitcoinCLI)(nil)
