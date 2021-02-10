490e79a68a33bf5b688f3f042883fba176f20227
Shunsuke Ise <ise@ebiiim.com>
2021-02-10 23:47:00 +0900

    btc: add Wallet

---
+++ btc/wallet.go
@@ -0,0 +1,161 @@
+package btc
+
+import (
+	"context"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"gocloud.dev/docstore"
+)
+
+func init() {
+	gob.Register(map[string]interface{}{})
+	gob.Register([]interface{}{})
+}
+
+// Errors
+var (
+	ErrCouldNotOpenWalletStore  = errors.New("ErrCouldNotOpenWalletStore")
+	ErrCouldNotCloseWalletStore = errors.New("ErrCouldNotCloseWalletStore")
+	ErrCouldNotLoadWallet       = errors.New("ErrCouldNotLoadWallet")
+	ErrCouldNotSaveWallet       = errors.New("ErrCouldNotSaveWallet")
+	ErrCouldNotGetNextUTXO      = errors.New("ErrCouldNotGetNextUTXO")
+	ErrCouldNotAddUTXO          = errors.New("ErrCouldNotAddUTXO")
+)
+
+type Wallet interface {
+	NextUTXO() (txid []byte, addr string, err error)
+	AddUTXO(txid []byte, addr string) error
+	io.Closer
+}
+
+var _ Wallet = (*DocstoreWallet)(nil)
+
+type utxo struct {
+	TXID []byte
+	Addr string
+}
+
+type utxosDoc struct {
+	DocKey string `docstore:"dockey"`
+	UTXOs  []utxo `docstore:"utxos"`
+}
+
+type DocstoreWallet struct {
+	q      []utxo
+	dockey string
+
+	conn string
+	coll *docstore.Collection
+}
+
+func (w *DocstoreWallet) enqueue(v utxo) {
+	w.q = append(w.q, v)
+}
+
+func (w *DocstoreWallet) dequeue() (utxo, error) {
+	if len(w.q) == 0 {
+		return utxo{}, errors.New("empty queue")
+	}
+	v := w.q[0]
+	w.q = w.q[1:]
+	return v, nil
+}
+
+func (w *DocstoreWallet) load() error {
+	doc := &utxosDoc{
+		DocKey: w.dockey,
+	}
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	if err := w.coll.Get(ctx, doc); err != nil {
+		return fmt.Errorf("%w (%v)", ErrCouldNotLoadWallet, err)
+	}
+	w.q = doc.UTXOs
+	return nil
+}
+
+func (w *DocstoreWallet) save() error {
+	doc := &utxosDoc{
+		DocKey: w.dockey,
+		UTXOs:  w.q,
+	}
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	if err := w.coll.Put(ctx, doc); err != nil {
+		return fmt.Errorf("%w (%v)", ErrCouldNotSaveWallet, err)
+	}
+	return nil
+}
+
+func (w *DocstoreWallet) open() error {
+	coll, err := docstore.OpenCollection(context.Background(), w.conn)
+	if err != nil {
+		return fmt.Errorf("%w (%v)", ErrCouldNotOpenWalletStore, err)
+	}
+	w.coll = coll
+	return nil
+}
+
+// MustNewDocstoreWallet initializes a DocstoreWallet,
+// panics if failed to access datastore.
+//
+// Parameters:
+//   - conn sets connection string.
+//   - dockey sets document name.
+//
+// DocstoreWallet puts all UTXOs in a single document specified by name.
+// TODO: put all UTXOs on every save is too heavy...
+func MustNewDocstoreWallet(conn string, dockey string) *DocstoreWallet {
+	w := &DocstoreWallet{
+		q:      nil,
+		dockey: dockey,
+		conn:   conn,
+		coll:   nil,
+	}
+	if err := w.open(); err != nil {
+		panic(fmt.Sprintf("%v conn=%s", err, conn))
+	}
+	if err := w.load(); err != nil {
+		log.Printf("MustNewDocstoreWallet: dockey=%s not found, create a new doc\n", dockey)
+	}
+	return w
+}
+
+func (w *DocstoreWallet) Close() error {
+	if err := w.save(); err != nil {
+		return fmt.Errorf("%w (%v)", ErrCouldNotCloseWalletStore, err)
+	}
+	if err := w.coll.Close(); err != nil {
+		return fmt.Errorf("%w (%v)", ErrCouldNotCloseWalletStore, err)
+	}
+	return nil
+}
+
+func (w *DocstoreWallet) NextUTXO() (txid []byte, addr string, err error) {
+	v, err := w.dequeue()
+	if err != nil {
+		return nil, "", fmt.Errorf("%w, %v", ErrCouldNotGetNextUTXO, err)
+	}
+	// Save on every NextUTXO call.
+	if err := w.save(); err != nil {
+		return nil, "", fmt.Errorf("%w, %v", ErrCouldNotGetNextUTXO, err)
+	}
+	return v.TXID, v.Addr, nil
+}
+
+func (w *DocstoreWallet) AddUTXO(txid []byte, addr string) error {
+	w.enqueue(utxo{
+		TXID: txid,
+		Addr: addr,
+	})
+	// Save on every AddUTXO call.
+	if err := w.save(); err != nil {
+		return fmt.Errorf("%w, %v", ErrCouldNotAddUTXO, err)
+	}
+	return nil
+}
---
+++ btc/wallet_test.go
@@ -0,0 +1,141 @@
+package btc_test
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/ebiiim/btcgw/btc"
+	"github.com/ebiiim/btcgw/util"
+
+	_ "gocloud.dev/docstore/memdocstore"
+)
+
+//
+// Do not parallelize DocstoreWallet tests because memdocstore is NOT thread-safe.
+//
+
+// Assumes tests will be run from package root.
+var (
+	testdb1 = "testdata/wallet1.db"
+	conn1   = "mem://wallet_test/dockey?filename=" + testdb1
+	conn2   = "mem://wallet_test/dockey"
+)
+
+var (
+	dockey = "abc"
+	wtx1   = util.MustDecodeHexString("123456")
+	waddr1 = "walletaddr0001"
+	wtx2   = util.MustDecodeHexString("654321")
+	waddr2 = "walletaddr0002"
+	wtx3   = util.MustDecodeHexString("0123456789abcdef0123456789abcdef")
+	waddr3 = "walletaddr0003"
+)
+
+func TestDocstoreWallet_UTXOs(t *testing.T) {
+	utxos1 := btc.MustNewDocstoreWallet(conn2, dockey)
+
+	// dequeue from [] -> [] err
+	txid, addr, err := utxos1.NextUTXO()
+	if txid != nil || addr != "" || !errors.Is(err, btc.ErrCouldNotGetNextUTXO) {
+		t.Errorf("want empty but got txid=%v, addr=%v", txid, addr)
+		t.Skip()
+	}
+	// enqueue utxo1 to [] -> [utxo1]
+	if err := utxos1.AddUTXO(wtx1, waddr1); err != nil {
+		t.Error(err)
+		t.Skip()
+	}
+	// dequeue from [utxo1] -> [] utxo1
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx1) != 0 || addr != waddr1 || err != nil {
+		t.Error("1")
+		t.Skip()
+	}
+	// dequeue from [] -> [] err
+	txid, addr, err = utxos1.NextUTXO()
+	if txid != nil || addr != "" || !errors.Is(err, btc.ErrCouldNotGetNextUTXO) {
+		t.Errorf("want empty but got txid=%v, addr=%v", txid, addr)
+		t.Skip()
+	}
+	// enqueue utxo1 to [] -> [utxo1]
+	// enqueue utxo2 to [] -> [utxo1, utxo2]
+	// enqueue utxo3 to [] -> [utxo1, utxo2, utxo3]
+	err1 := utxos1.AddUTXO(wtx1, waddr1)
+	err2 := utxos1.AddUTXO(wtx2, waddr2)
+	err3 := utxos1.AddUTXO(wtx3, waddr3)
+	if err1 != nil || err2 != nil || err3 != nil {
+		t.Error("2")
+		t.Skip()
+	}
+	// dequeue from [utxo1, utxo2, utxo3] -> [utxo2, utxo3] utxo1
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx1) != 0 || addr != waddr1 || err != nil {
+		t.Error("3")
+		t.Skip()
+	}
+	// dequeue from [utxo2, utxo3] -> [utxo3] utxo2
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx2) != 0 || addr != waddr2 || err != nil {
+		t.Error("4")
+		t.Skip()
+	}
+	// dequeue from [utxo3] -> [] utxo3
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx3) != 0 || addr != waddr3 || err != nil {
+		t.Error("5")
+		t.Skip()
+	}
+	if err := utxos1.Close(); err != nil {
+		t.Error(err)
+		t.Skip()
+	}
+}
+
+func TestNewDocstoreWallet(t *testing.T) {
+	utxos1 := btc.MustNewDocstoreWallet(conn1, dockey)
+	// dequeue from [utxo1, utxo2, utxo3] -> [utxo2, utxo3] utxo1
+	// dequeue from [utxo2, utxo3] -> [utxo3] utxo2
+	// dequeue from [utxo3] -> [] utxo3
+	// dequeue from [] -> [] err
+	// enqueue utxo1 to [] -> [utxo1]
+	// enqueue utxo2 to [utxo1] -> [utxo1, utxo2]
+	// enqueue utxo3 to [utxo1, utxo2] -> [utxo1, utxo2, utxo3]
+	txid, addr, err := utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx1) != 0 || addr != waddr1 || err != nil {
+		t.Errorf("1 txid=%v, addr=%v, err=%v", txid, addr, err)
+		t.Skip()
+	}
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx2) != 0 || addr != waddr2 || err != nil {
+		t.Error("2")
+		t.Skip()
+	}
+	txid, addr, err = utxos1.NextUTXO()
+	if bytes.Compare(txid, wtx3) != 0 || addr != waddr3 || err != nil {
+		t.Error("3")
+		t.Skip()
+	}
+	txid, addr, err = utxos1.NextUTXO()
+	if txid != nil || addr != "" || !errors.Is(err, btc.ErrCouldNotGetNextUTXO) {
+		t.Errorf("want empty but got txid=%v, addr=%v", txid, addr)
+		t.Skip()
+	}
+	err1 := utxos1.AddUTXO(wtx1, waddr1)
+	err2 := utxos1.AddUTXO(wtx2, waddr2)
+	err3 := utxos1.AddUTXO(wtx3, waddr3)
+	if err1 != nil || err2 != nil || err3 != nil {
+		t.Error("4")
+		t.Skip()
+	}
+	utxos2 := btc.MustNewDocstoreWallet(conn1, "XXX")
+	if txid, addr, err := utxos2.NextUTXO(); txid != nil || addr != "" || !errors.Is(err, btc.ErrCouldNotGetNextUTXO) {
+		t.Errorf("want empty but got txid=%v, addr=%v", txid, addr)
+		t.Skip()
+	}
+	// No need to close utxos2 because it's same with utxos1.
+	if err := utxos1.Close(); err != nil {
+		t.Error(err)
+		t.Skip()
+	}
+}
